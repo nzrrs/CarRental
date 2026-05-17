b74e443727d6467a87e9188e2536852000e1b27b
nzrrs <god.nizaryt10@gmail.com>
2026-05-17 22:48:25 +0100

    fix(animation-banner): Fixed animation jitter and make animation stop when hovering on the banner

--- frontend/src/pages/public/home/BrandsAnimation.jsx
+++ frontend/src/pages/public/home/BrandsAnimation.jsx
@@ -1,53 +1,40 @@
-import React from "react";
+const brands = [
+  "bmw",
+  "audi",
+  "mercedes",
+  "toyota",
+  "volkswagen",
+  "renault",
+  "dacia",
+  "peugeot",
+  "ford",
+  "hyundai",
+  "kia",
+  "jeep",
+  "nissan",
+  "honda",
+  "fiat",
+  "landrover",
+  "ferrari",
+  "lamborghini",
+];
 
-function BrandsAnimation() {
+export default function BrandsAnimation() {
   return (
-    <div className="container banner">
-      <div className="container track">
-
-        {/* FIRST LOOP */}
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/bmw.svg" alt="BMW" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/audi.svg" alt="Audi" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/mercedes.svg" alt="Mercedes" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/toyota.svg" alt="Toyota" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/volkswagen.svg" alt="Volkswagen" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/renault.svg" alt="Renault" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/dacia.svg" alt="Dacia" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/peugeot.svg" alt="Peugeot" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/ford.svg" alt="Ford" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/hyundai.svg" alt="Hyundai" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/kia.svg" alt="Kia" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/jeep.svg" alt="Jeep" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/nissan.svg" alt="Nissan" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/honda.svg" alt="Honda" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/fiat.svg" alt="Fiat" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/landrover.svg" alt="Land Rover" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/ferrari.svg" alt="Ferrari" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/lamborghini.svg" alt="Lamborghini" />
-
-        {/* SECOND LOOP (duplicate for smooth infinite scroll) */}
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/bmw.svg" alt="BMW" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/audi.svg" alt="Audi" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/mercedes.svg" alt="Mercedes" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/toyota.svg" alt="Toyota" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/volkswagen.svg" alt="Volkswagen" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/renault.svg" alt="Renault" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/dacia.svg" alt="Dacia" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/peugeot.svg" alt="Peugeot" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/ford.svg" alt="Ford" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/hyundai.svg" alt="Hyundai" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/kia.svg" alt="Kia" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/jeep.svg" alt="Jeep" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/nissan.svg" alt="Nissan" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/honda.svg" alt="Honda" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/fiat.svg" alt="Fiat" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/landrover.svg" alt="Land Rover" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/ferrari.svg" alt="Ferrari" />
-        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/lamborghini.svg" alt="Lamborghini" />
-
-      </div>
+   <div className="container banner">
+  <div className="container track">
+    <div className="group-banner">
+      {brands.map((b, i) => (
+        <img key={i+1} src={`https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/${b}.svg`} alt={b} />
+      ))}
     </div>
+
+    <div className="group-banner">
+      {brands.map((b, i) => (
+        <img key={i+1} src={`https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/${b}.svg`} alt={b} />
+      ))}
+    </div>
+  </div>
+</div>
   );
 }
-
-export default BrandsAnimation;
